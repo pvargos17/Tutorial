0315189e12de924d55a483a514ab0144ec5db772
Patrikc Vargos <pvargos17@gmail.com>
2018-11-24 17:37:52 -0600

    finished

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/patrickvargos/Documents/codingnomads/Week_11/tutorial/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
